7328c13d5e6967de10ee65932b4e14096941229c
Clint Checketts <clintchecketts@churchofjesuschrist.org>
2019-10-17 11:32:31 -0600

    Add example fixutre with comment

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,18 @@
 describe('page', () => {
-  it('works', () => {})
+  it('works', () => {
+		cy.visit("https://example.cypress.io/commands/files")
+
+		cy.server()
+		cy.fixture('example.json').as('comment')
+		cy.route('GET', 'comments', '@comment').as('getComment')
+
+		// we have code that gets a comment when
+		// the button is clicked in scripts.js
+		cy.get('.fixture-btn').click()
+
+		cy.wait('@getComment').its('responseBody')
+		  .should('have.property', 'name')
+		  .and('include', 'Using fixtures to represent data')
+        
+  })
 })
