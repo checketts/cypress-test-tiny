e2da55e79437d4a7abf7fc1b2c7716c131cfda71
Clint Checketts <clintchecketts@churchofjesuschrist.org>
2019-10-22 09:38:05 -0600

    CHange to JS fixture directly to allow comments

---
+++ cypress/fixtures/example.js
@@ -0,0 +1,6 @@
+export const example = {
+  name: "Using fixtures to represent data",
+  //Email should never be blank or null
+  email: "hello@cypress.io",
+  body: "Fixtures are a great way to mock data for responses to routes"
+}
--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,18 +1,19 @@
+import {example} from "../fixtures/example"
+
 describe('page', () => {
   it('works', () => {
 		cy.visit("https://example.cypress.io/commands/files")
 
 		cy.server()
-		cy.fixture('example.json5').as('comment')
-		cy.route('GET', 'comments/*', '@comment').as('getComment')
+		cy.route('GET', 'comments/*', example).as('getComment')
 
 		// we have code that gets a comment when
 		// the button is clicked in scripts.js
 		cy.get('.fixture-btn').click()
 
 		cy.wait('@getComment').its('responseBody')
-		  // .should('have.property', 'name')
-		  .should('include', 'Using fixtures to represent data')
+		  .should('have.property', 'name')
+		  .and('include', 'Using fixtures to represent data')
         
   })
 })
