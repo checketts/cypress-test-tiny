0229f7543c66b84e013b04efd5a5d7e1ddec3ef6
Clint Checketts <clintchecketts@churchofjesuschrist.org>
2019-10-22 09:17:02 -0600

    Include workaround as noted in https://github.com/cypress-io/cypress/issues/5395#issuecomment-544971131

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -3,16 +3,16 @@ describe('page', () => {
 		cy.visit("https://example.cypress.io/commands/files")
 
 		cy.server()
-		cy.fixture('example.json').as('comment')
-		cy.route('GET', 'comments', '@comment').as('getComment')
+		cy.fixture('example.json5').as('comment')
+		cy.route('GET', 'comments/*', '@comment').as('getComment')
 
 		// we have code that gets a comment when
 		// the button is clicked in scripts.js
 		cy.get('.fixture-btn').click()
 
 		cy.wait('@getComment').its('responseBody')
-		  .should('have.property', 'name')
-		  .and('include', 'Using fixtures to represent data')
+		  // .should('have.property', 'name')
+		  .should('include', 'Using fixtures to represent data')
         
   })
 })
